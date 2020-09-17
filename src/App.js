b1fae1a3ac424c1f3fc57be17745847bccd5bcf6
Filip Barl <filip.barl@gmail.com>
2020-09-17 10:52:28 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React from "react";
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  margin: 50px auto;
+  text-align: center;
+`;
+
+const AppContainer = styled.div`
+  color: ${props => props.theme.colors.black};
+  font-size: ${props => props.theme.fontSizes.normal};
+  display: inline-block;
+  padding: 20px;
+`;
+
+const Header = styled.h1`
+  font-size: ${props => props.theme.fontSizes.huge};
+
+  small {
+    font-size: ${props => props.theme.fontSizes.large};
+  }
+`;
+
+const SlothImage = styled.img.attrs({
+  src: "../sloth.png"
+})`
+  margin-bottom: 20px;
+  width: 300px;
+`;
+
+const Footer = styled.div``;
+
+const FooterItem = styled.span`
+  padding: 7px;
+`;
+
+const Link = styled.a`
+  color: ${props => props.theme.colors.darkGray};
+  text-decoration: none;
+
+  &:hover {
+    color: ${props => props.theme.colors.black};
+  }
+`;
+
+class App extends React.Component {
+  render() {
+    return (
+      <AppWrapper>
+        <AppContainer>
+          <Header>
+            Filip Barl{" "}
+            <small>
+              <Link href="https://github.com/fbarl">@fbarl</Link>
+            </small>
+          </Header>
+          <SlothImage />
+          <Footer>
+            <FooterItem>
+              <Link href="https://github.com/fbarl">[ github ]</Link>
+            </FooterItem>
+            <FooterItem>
+              <Link href="mailto:filip.barl@gmail.com">[ email ]</Link>
+            </FooterItem>
+          </Footer>
+        </AppContainer>
+      </AppWrapper>
+    );
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    black: "#1a1a1a",
+    darkGray: "#555"
+  },
+  fontSizes: {
+    huge: "2rem",
+    large: "1.375rem",
+    normal: "1.125rem"
+  }
+};
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
